242cd979165b2a94e3e0d63d6040b36298de86c2
javierbenavides <javierhbr@gmail.com>
2025-06-14 16:36:42 -0500

    fix: Update Node.js version matrix to only include version 20

--- src/core/errors.js
+++ src/core/errors.js
@@ -1,31 +1,34 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AuthenticationError = exports.ValidationError = exports.HttpError = void 0;
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.AuthenticationError =
+  exports.ValidationError =
+  exports.HttpError =
+    void 0;
 class HttpError extends Error {
-    status;
-    code;
-    details;
-    constructor(status, message, code, details) {
-        super(message);
-        this.status = status;
-        this.code = code;
-        this.details = details;
-        this.name = 'HttpError';
-    }
+  status;
+  code;
+  details;
+  constructor(status, message, code, details) {
+    super(message);
+    this.status = status;
+    this.code = code;
+    this.details = details;
+    this.name = 'HttpError';
+  }
 }
 exports.HttpError = HttpError;
 class ValidationError extends HttpError {
-    constructor(message, details) {
-        super(400, message, 'VALIDATION_ERROR', details);
-        this.name = 'ValidationError';
-    }
+  constructor(message, details) {
+    super(400, message, 'VALIDATION_ERROR', details);
+    this.name = 'ValidationError';
+  }
 }
 exports.ValidationError = ValidationError;
 class AuthenticationError extends HttpError {
-    constructor(message = 'Unauthorized') {
-        super(401, message, 'AUTHENTICATION_ERROR');
-        this.name = 'AuthenticationError';
-    }
+  constructor(message = 'Unauthorized') {
+    super(401, message, 'AUTHENTICATION_ERROR');
+    this.name = 'AuthenticationError';
+  }
 }
 exports.AuthenticationError = AuthenticationError;
 //# sourceMappingURL=errors.js.map
